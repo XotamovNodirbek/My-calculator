5c9177273f65a4ab312cfca425e7a1f4a5029a52
XotamovNodirbek <148562210+XotamovNodirbek@users.noreply.github.com>
2023-10-20 17:49:13 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+
+function Solve(val) {
+   var v = document.getElementById('res');
+   v.value += val;
+}
+function Result() {
+   var num1 = document.getElementById('res').value;
+   var num2 = eval(num1);
+   document.getElementById('res').value = num2;
+}
+function Clear() {
+   var inp = document.getElementById('res');
+   inp.value = '';
+   document.getElementById('res')
+}
+function Back() {
+   var ev = document.getElementById('res');
+   ev.value = ev.value.slice(0, -1);
+}
